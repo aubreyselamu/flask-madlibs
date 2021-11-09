fe2343a82bf41a8cf21316ba61696d95375eb87c
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-11-08 19:27:37 -0500

    added docstrings

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from stories import story
 
 @app.route('/')
 def get_prompt():
+    '''Generate form words in prompt'''
+
     prompts = story.prompts
     return render_template('questions.html', prompts=prompts)
 
 @app.route('/story')
 def get_text():
+    '''Show story result'''
+    
     text = story.generate(request.args)
     return render_template('stories.html', text=text)
